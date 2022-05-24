4db3278d07b2f60a7987a0828fbdd1b16175ccdf
starxingchenc <1340364178@qq.com>
2022-05-24 20:59:30 +0800

    1

--- testing/test.sh
+++ testing/test.sh
@@ -1,7 +1,4 @@
-ghc --make -O2 Main.hs
-sleep 1s
+ghc --make Main.hs
 time ./Main <./sample/test >./sample/test.json
-sleep 1s
 time ./Main <./sample/test1 >./sample/test1.json
-sleep 1s
 time ./Main <./sample/pao >./sample/pao.json
