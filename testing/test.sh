5b051ecf74df0c5db3e29dd974dc2628b814cfe9
starxingchenc <1340364178@qq.com>
2022-05-24 21:24:19 +0800

    add

--- testing/test.sh
+++ testing/test.sh
@@ -1,4 +1,5 @@
 ghc --make -O2 Main.hs
-time runhaskell Main.hs <./sample/test >./sample/test.json
-time runhaskell Main.hs <./sample/test1 >./sample/test1.json
-time runhaskell Main.hs <./sample/pao >./sample/pao.json
+time ./main <./sample/test >./sample/test.json
+time ./main <./sample/test1 >./sample/test1.json
+time ./main <./sample/pao >./sample/pao.json
+time ./main <./sample/fukugou >./sample/fukugou.json
