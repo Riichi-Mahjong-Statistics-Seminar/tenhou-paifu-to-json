a2a30adeb80e7c8c58dbb4ced92a964ed991e85e
starxingchenc <1340364178@qq.com>
2022-06-01 21:31:50 +0800

    fix

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-ghc --make -O2 ./src/Main.hs
+cd src
+ghc --make -O2 ./Main.hs
+cd ..
 time ./src/Main <./samples/test     >./samples/test.json
 time ./src/Main <./samples/yakumann >./samples/yakumann.json
 time ./src/Main <./samples/pao      >./samples/pao.json
